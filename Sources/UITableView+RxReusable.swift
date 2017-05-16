19f00d0cfd89a7e336980594e9cb73d5fbc5abdd
Suyeol Jeon <devxoul@gmail.com>
2017-05-16 19:48:36 +0900

    Swizzle didMoveToWindow instead of init

--- Sources/UITableView+RxReusable.swift
+++ Sources/UITableView+RxReusable.swift
@@ -11,33 +11,37 @@ import UIKit
 import RxCocoa
 
 #if os(iOS)
+private var didSubscribeKey = "didSubscribe"
+
 extension UITableView {
 
   open override static func initialize() {
     guard self === UITableView.self else { return }
     self._rxreusable_swizzle(
-      #selector(UITableView.init(frame:style:)),
-      #selector(UITableView._rxreusable_init(frame:style:))
+      #selector(UITableView.didMoveToWindow),
+      #selector(UITableView._rxreusable_didMoveToWindow)
     )
   }
 
-  func _rxreusable_init(frame: CGRect, style: UITableViewStyle) -> UITableView {
-    let instance = self._rxreusable_init(frame: frame, style: style)
-    _ = instance.rx.willDisplayCell
-      .takeUntil(instance.rx.deallocated)
+  func _rxreusable_didMoveToWindow() {
+    self._rxreusable_didMoveToWindow()
+    guard self.associatedObject(for: &didSubscribeKey) as? Bool != true else { return }
+    self.setAssociatedObject(true, for: &didSubscribeKey)
+
+    _ = self.rx.willDisplayCell
+      .takeUntil(self.rx.deallocated)
       .subscribe(onNext: { cell, indexPath in
         cell.traverseSubviews { view in
           (view as? UITableViewCell)?.rx.willDisplaySubject.onNext(indexPath)
         }
       })
-    _ = instance.rx.didEndDisplayingCell
-      .takeUntil(instance.rx.deallocated)
+    _ = self.rx.didEndDisplayingCell
+      .takeUntil(self.rx.deallocated)
       .subscribe(onNext: { cell, indexPath in
         cell.traverseSubviews { view in
           (view as? UITableViewCell)?.rx.didEndDisplayingSubject.onNext(indexPath)
         }
       })
-    return instance
   }
 
 }
